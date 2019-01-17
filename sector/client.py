370f58ea2c610d59717010267ca174bbe8fd441d
Yaz Khoury <yaz.khoury@gmail.com>
2019-01-16 19:12:25 -0500

    feat: add custom sector clas

--- sector/client.py
+++ sector/client.py
@@ -21,17 +21,17 @@ class Client(Sector):
         url = """
 		https://www.ethernodes.org/network/1/data?draw=1&columns[0][data]=id&columns[0][name]=&columns[0][searchable]=true&columns[0][orderable]=true&columns[0][search][value]=&columns[0][search][regex]=false&columns[1][data]=host&columns[1][name]=&columns[1][searchable]=true&columns[1][orderable]=true&columns[1][search][value]=&columns[1][search][regex]=false&columns[2][data]=port&columns[2][name]=&columns[2][searchable]=true&columns[2][orderable]=true&columns[2][search][value]=&columns[2][search][regex]=false&columns[3][data]=country&columns[3][name]=&columns[3][searchable]=true&columns[3][orderable]=true&columns[3][search][value]=&columns[3][search][regex]=false&columns[4][data]=clientId&columns[4][name]=&columns[4][searchable]=true&columns[4][orderable]=true&columns[4][search][value]=&columns[4][search][regex]=false&columns[5][data]=client&columns[5][name]=&columns[5][searchable]=true&columns[5][orderable]=true&columns[5][search][value]=&columns[5][search][regex]=false&columns[6][data]=clientVersion&columns[6][name]=&columns[6][searchable]=true&columns[6][orderable]=true&columns[6][search][value]=&columns[6][search][regex]=false&columns[7][data]=os&columns[7][name]=&columns[7][searchable]=true&columns[7][orderable]=true&columns[7][search][value]=&columns[7][search][regex]=false&columns[8][data]=lastUpdate&columns[8][name]=&columns[8][searchable]=true&columns[8][orderable]=true&columns[8][search][value]=&columns[8][search][regex]=false&order[0][column]=0&order[0][dir]=asc&start=0&length=1000000&search[value]=&search[regex]=false&_=1545071939943
       	"""
-
-		response = requests.get(url)
-		response_hash = json.loads(response.text).get('data')
-		df = pd.DataFrame.from_dict(response_hash)
-		client_raw = df.groupby('client').nunique()
-		client = client_raw['id']
-		client = client.sort_values()
-		client_data = np.array(client)
-		gini_object = Gini(client_data)
-		self.gini = gini_object.get_gini()
-		self.plot = self.generate_lorenz_curve()
+        
+        response = requests.get(url)
+        response_hash = json.loads(response.text).get('data')
+        df = pd.DataFrame.from_dict(response_hash)
+        client_raw = df.groupby('client').nunique()
+        client = client_raw['id']
+        client = client.sort_values()
+        client_data = np.array(client)
+        gini_object = Gini(client_data)
+        self.gini = gini_object.get_gini()
+        self.plot = self.generate_lorenz_curve()
 
         def generate_lorenz_curve(self):
             file_name = f'{self.currency}_client_gini_{self.uuid}'
--- sector/sector.py
+++ sector/sector.py
@@ -1,5 +1,10 @@
+from sector import Sector
+from analysis import Gini, LorenzPlot
+from uuid import uuid
+
 class Sector(object):
     def __init__(self):
+        self.uuid = uuid.uuid4()
         self.data = None
         self.plot = None
         self.title = None
@@ -14,10 +19,24 @@ class Sector(object):
         if self.plot:
             return self.plot
 
-class Geography(Sector):
-    def __init__(self):
-        pass
+class CustomSector(object):
+    def __init__(self, data, currency, sector_type):
+        self.uuid = uuid.uuid4()
+        self.data = data
+        self.gini = self.generate_gini()
+        self.currency = currency
+        self.type = sector_type
+        self.plot = self.generate_plot_url()
+        self.nakamoto = self.generate_nakamoto()
 
-class Client(Sector):
-    def __init__(self):
-        pass
+    def generate_gini(self):
+        gini_object = Gini(self.data)
+        gini = gini_object.get_gini()
+        return gini
+
+    def generate_plot_url(self):
+        file_name = f'{self.currency}_{self.type}_gini_{self.uuid}'
+        lorenz_object = LorenzPlot(self.plotly_username, self.plotly_api, self.contributor_data, file_name)
+        plot_url = lorenz_object.plotly_url()
+        return plot_url
+
